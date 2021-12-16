a45feb885739d93be7ea15befe9b584121c346c2
Steph Heins <stephanie_heins@yahoo.com>
2021-12-15 22:02:25 -0800

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import '../styles/NavTabs.css';
+
+export default function Footer() {
+    return (
+        <div>
+              <Navbar bg="dark" variant="dark">
+                <Container className="justify-content-center">
+                    <Nav>
+                        <Nav.Link>©2021</Nav.Link>
+                        <Nav.Link href="" id="github">Github</Nav.Link>
+                        <Nav.Link href="" id="linkedin">LinkedIn</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
+)};
--- src/pages/PortfolioContainer.js
+++ src/pages/PortfolioContainer.js
@@ -1,9 +1,12 @@
 import NavTabs from './NavTabs'
+import Footer from '../components/Footer'
 
 export default function PortfolioContainer() {
     return (
         <div>
             <NavTabs />
+            Test
+            <Footer />
         </div>
     )
 }
