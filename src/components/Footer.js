7686799c5117b778499ec727b287104322afed6c
Steph Heins <stephanie_heins@yahoo.com>
2021-12-16 20:02:26 -0800

    slight layout changes and forgot footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ export default function Footer() {
                 <Container className="justify-content-center">
                     <Nav>
                         <Nav.Link>©2021</Nav.Link>
-                        <Nav.Link href="" id="github">Github</Nav.Link>
-                        <Nav.Link href="" id="linkedin">LinkedIn</Nav.Link>
+                        <Nav.Link href="https://github.com/StephanieHeins" id="github">Github</Nav.Link>
+                        <Nav.Link href="https://www.linkedin.com/in/stephanieheins/" id="linkedin">LinkedIn</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
